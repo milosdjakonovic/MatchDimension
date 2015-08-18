95ef416046dcffdf2bcbe8b1c104ec9625b75998
Miloš Đakonović <milos@djakonovic.net>
2015-08-18 22:49:36 +0200

    Major update updated API, test file and README.md

--- README.md
+++ README.md
@@ -4,29 +4,31 @@ Cross browser Javascript media queries
 
 Small library aimed to provide **Javascript media queries** accross all browsers, event those most standard-noncompliant that do not support CSS media queries.
 * No dependancy
-* Tested in IE6+
-
+* Lightweight (<=1KB minified) and very fast (no string processing, method names corespondes media queries)
+* Works accross every A and not so A grade browser (even in IE6 and Android 2.1 browser)
+* Takes possible window resize or orientation change into count - gives accurate result
+* Simple to write, less chars
 
 
 ###Usage:
 
 ```javascript
 
-if ( mtchD.minWidth(480).test() ) {
+if ( mtchD.minWidth(480).t() ) {
   // code for viewport width => 480px
   //equivalent for @media(min-width:480px)
 }
-if ( mtchD.maxWidth(1600).test() ) {
+if ( mtchD.maxWidth(1600).t() ) {
   // code for viewport width <= 1600px
   //equivalent for @media(max-width:1600px)
 }
 
-if ( mtchD.minHeight(480).test() ) {
+if ( mtchD.minHeight(480).t() ) {
   // code for viewport height => 480px
   //equivalent for @media(min-height:480px)
 }
 
-if ( mtchD.maxHeight(1600).test() ) {
+if ( mtchD.maxHeight(1600).t() ) {
   // code for viewport height <= 1600px
   //equivalent for @media(max-height:1600px)
 }
@@ -39,22 +41,22 @@ if ( mtchD.maxHeight(1600).test() ) {
 
 ```javascript
 
-if ( mtchD.minWidth(480).maxWidth(1200).test() ) {
+if ( mtchD.minWidth(480).maxWidth(1200).t() ) {
   //equivalent for @media (min-width:480px) and (max-width: 1200px)
 }
 
 
 ```
 
-#### (a very basic) orientation detection included
+#### Basic orientation detection included
 
 
 ```javascript
 
-if ( mtchD.portrait().minWidth(480).test() ) {
+if ( mtchD.portrait().minWidth(480).t() ) {
   //goes for portrait orientation and min width of 480px
 }
-if ( mtchD.landscape().minWidth(480).test() ) {
+if ( mtchD.landscape().minWidth(480).t() ) {
   //goes for landscape orientation and min width of 480px
 }
 
--- matchd.js
+++ matchd.js
@@ -4,29 +4,76 @@
 
 
 window.mtchD={
-    result : {
+
+    bool : true,
+    
+    w:0,
+    
+    h:0,
+    
+    setHW: function(){
+        this.w = Math.max(document.documentElement.clientWidth,  window.innerWidth  || 0);
+        this.h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    },    
         
-    },
-    w:Math.max(document.documentElement.clientWidth,  window.innerWidth  || 0),
-    h:Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
     minWidth : function(D){
-        var exp = ( this.w >= D );
-        this.result.minWidth = exp;
+        this.setHW();
+        
+        if (!this.bool) {
+            return this;
+        }
+        
+        if( this.w >= D ) {
+            this.bool = true;
+        } else {
+            this.bool = false;
+        }
+        
         return this;
     },
     maxWidth : function(D){
-        var exp = ( this.w <= D );
-        this.result.maxWidth = exp;
+        this.setHW();
+        
+        if (!this.bool) {
+            return this;
+        }
+        
+        if ( this.w <= D ) {
+            this.bool = true;
+        } else {
+            this.bool = false;
+        }
+        
         return this;
     },
     minHeight : function(D){
-        var exp = ( this.h >= D );
-        this.result.minHeight = exp;
+        this.setHW();
+        
+        if (!this.bool) {
+            return this;
+        }
+        
+        if( this.h >= D ) {
+            this.bool = true;
+        } else {
+            this.bool = false;
+        }
+
         return this;
     },
     maxHeight: function(D){
-        var exp = ( this.h <= D );
-        this.result.maxWidth = exp;
+        this.setHW();
+        
+        if (!this.bool) {
+            return this;
+        }
+        
+        if ( this.h <= D ) {
+            this.bool = true;
+        } else {
+            this.bool = false;
+        }
+        
         return this;
     },
     // both mtchD.landscape() and mtchD.portrait() provides
@@ -34,43 +81,41 @@ window.mtchD={
     // which may be inaccurate on some mobile devices
     // thus will be updated soon
     landscape: function(){
+        this.setHW();
         
-        if( (this.h < this.w) ) {
-            this.result.orientation = true;
+        if (!this.bool) {
             return this;
+        }
+        
+        if (this.h < this.w)  {
+            this.bool = true;
         } else {
-            this.result.orientation = false;
-            return this;
+            this.bool = false;
         }
         
         return this;
     },
     portrait: function(){
-
-        if ( (this.h > this.w) ) {
-            this.result.orientation = true;
-            return this;
-        } else {
-            this.result.orientation = false
-            return this;
-        }
-       
-    },
-    
-    test: function(){
-        //get instance of this.result
-        var r = this.result,
-        testedProps = [];
-        //flush previous result set
-        //required to run next test
-        this.result={};
+        this.setHW();
         
-        // Process results and returns T/F
-        for( x in r){
-            if (r[x]===false) {
-                return false;
-            }
+        if (!this.bool) {
+            return this;
         }
-        return true;
+        
+        if(this.h > this.w) {
+            this.bool = true;
+        } else {
+            this.bool = false;
+        }
+        
+        return this;        
+    },
+    t : function(){
+        var boolCopy = this.bool;
+        //reset for further use
+        this.bool = true;
+        return boolCopy;
     }
+    
+
 };
